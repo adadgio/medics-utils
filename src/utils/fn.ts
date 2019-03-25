f97b839e2e3109da6897a073d66e72160eb7e426
adadgio <romain.bruckert.esc@gmail.com>
2019-03-25 10:36:15 +0100

    Npm pre-publish commit

--- src/utils/fn.ts
+++ src/utils/fn.ts
@@ -5,16 +5,7 @@ export let fn =
 {
     getArgs(f: Function)
     {
-        // First match everything inside the function argument parens.
-        const args = func.toString().match(/function\s.*?\(([^)]*)\)/)[1]
-        
-        // Split the arguments string into an array comma delimited.
-        return args.split(',').map(function(arg) {
-            // Ensure no inline comments are parsed and trim the whitespace.
-            return arg.replace(/\/\*.*\*\//, '').trim();
-        }).filter(function(arg) {
-            // Ensure no undefined values are added.
-            return arg
-        })
+        const funStr = f.toString()
+        return funStr.slice(funStr.indexOf('(') + 1, funStr.indexOf(')')).match(/([^\s,]+)/g)
     },
 }
--- test/fn.spec.ts
+++ test/fn.spec.ts
@@ -3,10 +3,23 @@ const expect = require('chai').expect
 
 import { fn } from '../src/utils/fn'
 
-describe('Fn (functions utils) suite', () => {
+class Test {
+    zeMethod(name: string, age: number)
+    {
+        return 3
+    }
+}
 
-    it('should be equal to true', () => {
-        expect(true).to.equal(true)
+const instanceOfTest = new Test()
+const method = instanceOfTest.zeMethod
+
+const argNames = fn.getArgs(method)
+
+describe('Fn suite (function utils)', () => {
+
+    it('function arguments names must be "name" and "age"', () => {
+        expect(argNames[0]).to.equal('name')
+        expect(argNames[1]).to.equal('age')
     })
 
 
